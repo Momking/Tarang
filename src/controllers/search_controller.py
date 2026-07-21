0ed7c254d8d617ba8e58f46a3dc3194e7b77ec5f
Momking <nishant.dqy@gmail.com>
2026-07-21 18:23:50 +0530

    made search asynchronus

--- src/controllers/search_controller.py
+++ src/controllers/search_controller.py
@@ -1,4 +1,10 @@
+import threading
+from gi.repository import GLib
+
+
 class SearchController:
+
+    SEARCH_DELAY_MS = 50
 
     def __init__(
         self,
@@ -8,14 +14,65 @@ class SearchController:
         self.plugins = plugin_manager
         self.grid = grid
 
+        self.search_generation = 0
+        self.pending_search = None
+
     def initialize(self):
         self.search("")
 
+    def start_search(self, query):
+
+        self.pending_search = None
+
+        self.search_generation += 1
+
+        generation = self.search_generation
+
+        threading.Thread(
+            target=self.search_worker,
+            args=(query, generation),
+            daemon=True,
+        ).start()
+
+        return False
+
     def search(self, query):
+
+        if self.pending_search is not None:
+            GLib.source_remove(self.pending_search)
+
+        self.pending_search = GLib.timeout_add(
+            self.SEARCH_DELAY_MS,
+            self.start_search,
+            query,
+        )
+
+    def finish_search(
+        self,
+        generation,
+        results,
+    ):
+
+        if generation != self.search_generation:
+            return False
+
+        self.grid.set_results(results)
+
+        return False
+
+    def search_worker(
+        self,
+        query,
+        generation,
+    ):
 
         results = self.plugins.search(query)
 
-        self.grid.set_results(results)
+        GLib.idle_add(
+            self.finish_search,
+            generation,
+            results,
+        )
 
     def activate(self, result):
 
@@ -27,6 +84,6 @@ class SearchController:
 
     def move_next(self):
             self.grid.move_next()
-    
+
     def move_previous(self):
         self.grid.move_previous()
