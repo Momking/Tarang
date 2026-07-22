184b7cce6c88f0d4dacd3012d14fb9fc49d468bc
Momking <nishant.dqy@gmail.com>
2026-07-22 19:45:23 +0530

    modified file_index_service to implement dir recursive search, thread safety

--- src/services/file_index_service.py
+++ src/services/file_index_service.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import threading
 
+from gi.repository import Gio
+
+
 from models.file_info import FileInfo
 
 
@@ -24,6 +27,12 @@ class FileIndexService:
     def __init__(self):
         self.files: list[FileInfo] = []
 
+        self.monitors: dict[Path, Gio.FileMonitor] = {}
+
+        self.setup_monitors()
+
+        self.lock = threading.Lock()
+
         threading.Thread(
 
             target=self._build_index,
@@ -34,7 +43,7 @@ class FileIndexService:
 
     def _build_index(self):
 
-        self.files.clear()
+        new_files: list[FileInfo] = []
 
         for root in self.SEARCH_DIRS:
 
@@ -43,40 +52,170 @@ class FileIndexService:
 
             for path in root.rglob("*"):
 
-                if any(
-                    part in self.IGNORE
-                    for part in path.parts
-                ):
+                if any(part in self.IGNORE for part in path.parts):
                     continue
 
                 if not path.is_file():
                     continue
 
-                self.files.append(
-
+                new_files.append(
                     FileInfo(
                         path=path,
                         name=path.name,
                     )
-
                 )
 
-    def all_files(self):
-        return self.files
+        with self.lock:
+            self.files = new_files
 
-    @staticmethod
-    def score(
-        query,
-        name,
+    def all_files(self):
+        with self.lock:
+            return self.files.copy()
+
+    def setup_monitors(self):
+
+        for directory in self.SEARCH_DIRS:
+
+            if not directory.exists():
+                continue
+
+            for root in directory.rglob("*"):
+                    if root.is_dir():
+                        self.monitor_directory(root)
+
+            self.monitor_directory(directory)
+
+            monitor = (
+                Gio.File.new_for_path(
+                    str(directory)
+                )
+                .monitor_directory(
+                    Gio.FileMonitorFlags.NONE,
+                    None,
+                )
+            )
+
+            monitor.connect(
+                "changed",
+                self.on_changed,
+            )
+
+            self.monitors[directory] = monitor
+
+    def on_changed(
+        self,
+        monitor,
+        file,
+        other_file,
+        event,
+    ):
+        path_str = file.get_path()
+
+        if path_str is None:
+            return
+
+        path = Path(path_str)
+
+        if event == Gio.FileMonitorEvent.CREATED:
+
+            self.add_file(path)
+        elif event == Gio.FileMonitorEvent.DELETED:
+
+            self.remove_file(path)
+        elif event == Gio.FileMonitorEvent.RENAMED:
+
+            self.remove_file(path)
+
+            if other_file:
+
+                other_path = other_file.get_path()
+
+                if other_path is not None:
+                    self.add_file(Path(other_path))
+
+    def add_file(
+        self,
+        path: Path,
+    ):
+        if path.is_dir():
+            self.monitor_directory(path)
+            return
+
+        FileInfo(
+            name=path.name,
+            path=path,
+        )
+
+
+        if path.name.startswith("."):
+            return
+
+        if path.suffix in {
+
+            ".swp",
+            ".tmp",
+            ".part",
+
+        }:
+            return
+
+        with self.lock:
+            if any(
+                file.path == path
+                for file in self.files
+            ):
+                return
+
+            self.files.append(
+                FileInfo(
+                    name=path.name,
+                    path=path,
+                )
+            )
+
+    def remove_file(self, path: Path):
+
+        if path.is_dir():
+            self.unmonitor_directory(path)
+            return
+
+        with self.lock:
+            self.files = [
+                file
+                for file in self.files
+                if file.path != path
+            ]
+
+    def monitor_directory(self, directory: Path):
+
+        if directory in self.monitors:
+            return
+
+        monitor = (
+            Gio.File.new_for_path(str(directory))
+            .monitor_directory(
+                Gio.FileMonitorFlags.NONE,
+                None,
+            )
+        )
+
+        monitor.connect(
+            "changed",
+            self.on_changed,
+        )
+
+        self.monitors[directory] = monitor
+
+    def unmonitor_directory(
+        self,
+        directory: Path,
     ):
 
-        if name == query:
-            return 1000
+        monitor = self.monitors.pop(
+            directory,
+            None,
+        )
 
-        if name.startswith(query):
-            return 800
+        if monitor:
 
-        if query in name:
-            return 500
-
-        return 0
+            monitor.cancel()
