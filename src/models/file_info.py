1b867dfa0010d47aabbe9ff35fc845f42f2e71bc
Momking <nishant.dqy@gmail.com>
2026-07-23 09:26:26 +0530

    seralization logic added to the file_info model

--- src/models/file_info.py
+++ src/models/file_info.py
@@ -4,7 +4,20 @@ from pathlib import Path
 
 @dataclass(slots=True)
 class FileInfo:
-
     path: Path
-
     name: str
+
+    def to_dict(self):
+
+        return {
+            "path": str(self.path),
+            "name": self.name,
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+
+        return cls(
+            path=Path(data["path"]),
+            name=data["name"],
+        )
