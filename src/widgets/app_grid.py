44cf50b64b55d3d5b4d0dc199b9fb57448b0492b
Momking <nishant.dqy@gmail.com>
2026-07-24 10:51:17 +0530

    changed the flexbox to gridview

--- src/widgets/app_grid.py
+++ src/widgets/app_grid.py
@@ -1,6 +1,7 @@
-from gi.repository import Gtk, GObject
+from gi.repository import Gtk, GObject, Gio
 
 from widgets.app_card import AppCard
+from models.plugin_result_item import PluginResultItem
 
 
 class AppGrid(Gtk.ScrolledWindow):
@@ -19,95 +20,155 @@ class AppGrid(Gtk.ScrolledWindow):
         self.set_vexpand(True)
         self.set_hexpand(True)
 
-        self.flowbox = Gtk.FlowBox()
+        #--------------------------------------------------
+        # Initialize the store and selection model
 
-        self.flowbox.set_selection_mode(
-            Gtk.SelectionMode.NONE
+        self.store = Gio.ListStore.new(
+            PluginResultItem
         )
 
-        self.flowbox.set_max_children_per_line(6)
-        self.flowbox.set_min_children_per_line(3)
-        self.flowbox.set_row_spacing(16)
-        self.flowbox.set_column_spacing(16)
+        self.selection = Gtk.SingleSelection(
+            model=self.store
+        )
 
-        self.set_child(self.flowbox)
+        factory = Gtk.SignalListItemFactory()
 
-        self.selected_index = 0
-        self.cards = []
+        factory.connect(
+            "setup",
+            self.on_setup,
+        )
+
+        factory.connect(
+            "bind",
+            self.on_bind,
+        )
+
+        factory.connect(
+            "unbind",
+            self.on_unbind,
+        )
+
+        self.grid = Gtk.GridView(
+            model=self.selection,
+            factory=factory,
+        )
+
+        self.grid.set_enable_rubberband(False)
+
+        self.grid.set_max_columns(6)
+        self.grid.set_min_columns(3)
+
+        self.set_child(self.grid)
 
     def set_results(self, results):
 
-        self.clear()
+        self.result = results
 
-        self.cards.clear()
+        if results is None:
+            self.label.set_text("")
+            self.image.clear()
+            return
+
+        self.store.remove_all()
 
         for result in results:
-
-            card = AppCard()
-
-            card.set_result(result)
-
-            self.cards.append(card)
-
-            card.connect(
-                "activated",
-                self.on_card_activated,
+            self.store.append(
+                PluginResultItem(result)
             )
 
-            self.flowbox.insert(card, -1)
+        if self.store.get_n_items() > 0:
+            self.selection.set_selected(0)
 
-        self.selected_index = 0
-        self.update_selection()
+    def on_setup(
+        self,
+        factory,
+        list_item,
+    ):
 
-    def on_card_activated(self, card):
+        card = AppCard()
 
-        self.emit(
-            "app-activated",
-            card.result,
+        self.grid.connect(
+            "activate",
+            self.on_activate,
         )
+
+        list_item.set_child(card)
+
+    def on_bind(
+        self,
+        factory,
+        list_item,
+    ):
+
+        card = list_item.get_child()
+
+        plugin_result = list_item.get_item()
+
+        card.set_result(
+            plugin_result.result
+        )
+
+    def on_unbind(
+        self,
+        factory,
+        list_item,
+    ):
+        card = list_item.get_child()
+
+        card.set_result(None)
+
+    def get_selected_item(self):
+
+        return self.selection.get_selected_item()
+
+    def get_selected_result(self):
+
+        plugin_result = self.get_selected_item()
+
+        if plugin_result is None:
+            return None
+
+        return plugin_result.result
 
     def activate_selected(self):
 
-        if not self.cards:
+        result = self.get_selected_result()
+
+        if result is None:
             return
 
-        self.cards[
-            self.selected_index
-        ].on_clicked(None)
+        self.emit(
+            "app-activated",
+            result,
+        )
 
-    def update_selection(self):
+    def on_activate(self, grid, position):
 
-        for i, card in enumerate(self.cards):
+        item = self.store.get_item(position)
 
-            card.set_selected(
-                i == self.selected_index
-            )
+        self.emit(
+            "app-activated",
+            item.result,
+        )
 
     def move_next(self):
 
-        if not self.cards:
+        index = self.selection.get_selected()
+
+        if index == Gtk.INVALID_LIST_POSITION:
             return
 
-        self.selected_index = min(
-            self.selected_index + 1,
-            len(self.cards) - 1,
-        )
+        count = self.store.get_n_items()
 
-        self.update_selection()
+        if index < count - 1:
+            self.selection.set_selected(index + 1)
 
     def move_previous(self):
 
-        if not self.cards:
+        index = self.selection.get_selected()
+
+        if index == Gtk.INVALID_LIST_POSITION:
             return
 
-        self.selected_index = max(
-            self.selected_index - 1,
-            0,
-        )
-
-        self.update_selection()
-
-    def clear(self):
-
-        while (child := self.flowbox.get_first_child()) is not None:
-            self.flowbox.remove(child)
+        if index > 0:
+            self.selection.set_selected(index - 1)
