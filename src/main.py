0377efbe6fa74976db907e61d7a691841a129442
Momking <nishant.dqy@gmail.com>
2026-07-27 12:41:21 +0530

    modified the main file to add dock window

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ gi.require_version("Gtk", "4.0")
 gi.require_version("Gtk4LayerShell", "1.0")
 gi.require_version("GnomeDesktop", "4.0")
 
+from controllers.plugin_docker_controller import PluginDockController   #noqa
+
 from gi.repository import (
     Gtk,
     Gtk4LayerShell,
@@ -12,6 +14,9 @@ from pathlib import Path
 
 from widgets.launcher_window import LauncherWindow
 from services.theme_service import ThemeService
+from models.plugin_state import PluginState
+from widgets.docker_window import DockWindow
+
 
 class LauncherApplication(Gtk.Application):
     def __init__(self):
@@ -20,8 +25,26 @@ class LauncherApplication(Gtk.Application):
         )
 
     def do_activate(self):
-        window = LauncherWindow(self)
-        window.present()
+
+        self.plugin_state = PluginState()
+
+        self.launcher = LauncherWindow(
+            self,
+            self.plugin_state,
+        )
+
+        self.dock = DockWindow(
+            self,
+            self.plugin_state,
+        )
+
+        self.plugin_dock_controller = PluginDockController(
+            self.dock.plugin_dock,
+            self.plugin_state,
+        )
+
+        self.dock.present()
+        self.launcher.present()
 
         theme = ThemeService()
 
