ef7a63d36f68aa28beda2d9a5480328a4b83d973
Momking <nishant.dqy@gmail.com>
2026-07-27 12:48:04 +0530

    added a new Gtk layer-shell window creation for the dock

--- src/wayland/layer_shell.py
+++ src/wayland/layer_shell.py
@@ -1,5 +1,3 @@
-from ast import Gt
-
 import gi
 
 gi.require_version("Gtk", "4.0")
@@ -42,5 +40,30 @@ def setup_launcher(window):
 
     Gtk4LayerShell.set_margin(window, Gtk4LayerShell.Edge.TOP, 60)
     # Gtk4LayerShell.set_margin(window, Gtk4LayerShell.Edge.LEFT, 400)
+
+    Gtk4LayerShell.set_namespace(window, "tarang-launcher")
+
+def setup_docker(window):
+    Gtk4LayerShell.init_for_window(window)
+
+    Gtk4LayerShell.set_layer(
+        window,
+        Gtk4LayerShell.Layer.OVERLAY,
+    )
+
+    Gtk4LayerShell.set_keyboard_mode(
+        window,
+        Gtk4LayerShell.KeyboardMode.ON_DEMAND,
+    )
+
+    # window_width, window_height = get_window_size(window)
+    # window.set_default_size(window_width/2, window_height/8)
+
+    Gtk4LayerShell.set_anchor(window, Gtk4LayerShell.Edge.TOP, False)
+    Gtk4LayerShell.set_anchor(window, Gtk4LayerShell.Edge.LEFT, False)
+    Gtk4LayerShell.set_anchor(window, Gtk4LayerShell.Edge.BOTTOM, True)
+    Gtk4LayerShell.set_anchor(window, Gtk4LayerShell.Edge.RIGHT, False)
+
+    Gtk4LayerShell.set_margin(window, Gtk4LayerShell.Edge.BOTTOM, 100)
 
     Gtk4LayerShell.set_namespace(window, "tarang-launcher")
