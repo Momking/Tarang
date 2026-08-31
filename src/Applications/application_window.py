ccc940c47f35f014e75ca951887d4e291324c2e0
Momking <nishant.dqy@gmail.com>
2026-08-31 15:56:23 +0530

    modified to add application scrollable window to the main application window

--- src/Applications/application_window.py
+++ src/Applications/application_window.py
@@ -1,4 +1,10 @@
+from pathlib import Path    #noqa
+
 from gi.repository import Gtk
+
+from wayland.layer_shell import setup_launcher as setup_layer_shell
+from Applications.application_scrollable_window import ApplicationScrollableWindow
+
 
 class ApplicationWindow(Gtk.Window):
 
@@ -6,14 +12,26 @@ class ApplicationWindow(Gtk.Window):
         super().__init__()
         self.set_default_size(600, 400)
 
+        setup_layer_shell(self)
 
-def on_activate(app):
-    win = ApplicationWindow()
-    win.set_application(app)
-    win.present()
+        # Build layout
+        self.window = Gtk.Box(
+            orientation=Gtk.Orientation.VERTICAL,
+        )
 
+        self.set_child(self.window)
 
-if __name__ == "__main__":
-    app = Gtk.Application(application_id="com.example.CenteredScroll")
-    app.connect('activate', on_activate)
-    app.run(None)
+        # Header
+        header = Gtk.Label(label="Header\n")
+        self.window.append(header)
+
+        # Add a scrolled window
+        scrolled_window = ApplicationScrollableWindow()
+        scrolled_window.set_vexpand(True)
+        scrolled_window.set_hexpand(True) # Optional: ensures it fills left-to-right as well
+        scrolled_window.set_valign(Gtk.Align.FILL)
+        self.window.append(scrolled_window)
+
+        # Footer
+        footer = Gtk.Label(label="Footer")
+        self.window.append(footer)
